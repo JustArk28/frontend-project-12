1e5197cc867dcec513c30ddcbce2a21060851b88
JustArk28 <arkadiyarkadiy95@gmail.com>
2026-02-25 21:09:49 +0700

    changed ChannelsList and style

--- frontend/src/components/ChatPageContainer/ChannelsList.jsx
+++ frontend/src/components/ChatPageContainer/ChannelsList.jsx
@@ -19,50 +19,54 @@ const ChannelsList = ({
   const channels = useSelector((state) => state.channelsStore.channels);
   return (
     <div className="channel-list">
-      <Nav className="flex-column nav-pills nav-link">
-        {channels.map(({ id, name, removable }) => {
-          if (removable) {
-            return (
-              <div className="d-flex" key={id}>
-                <ButtonGroup>
-                  <Nav.Item
-                    key={id}
-                    onClick={() => (
-                      setCurrentChannelId(id),
-                      setInputFocus(true)
-                    )}
-                  >
-                    <Nav.Link className='channel-name' eventKey={id}># {name}</Nav.Link>
-                  </Nav.Item>
-
-                  <DropdownButton
-                    bsPrefix={currentChannelId === id ? "btn btn-secondary" : "btn btn-light"}                
-                    as={ButtonGroup}
-                    id="bg-nested-dropdown"
-                  >
-                    <Dropdown.Item id={id} onClick={handleRemove}>
-                      {t("dropdownBtn.removeBtn")}
-                    </Dropdown.Item>
-                    <Dropdown.Item id={id} onClick={handleRename}>
-                      {t("dropdownBtn.renameBtn")}
-                    </Dropdown.Item>
-                  </DropdownButton>
-                </ButtonGroup>
-              </div>
-            );
-          }
+      {/* <Nav className="flex-column nav-pills nav-link"> */}
+      {channels.map(({ id, name, removable }) => {
+        if (removable) {
           return (
-            <Button
-              key={id}
-              type='submit'              
-              eventkey={id}
-              onClick={() => (setCurrentChannelId(id), setInputFocus(true))}
-            >
-              # {name}
-            </Button>
+            <div className="d-flex" key={id}>
+              <ButtonGroup>
+                <Button
+                  key={id}
+                  type="submit"
+                  eventkey={id}
+                  className="channel-name"
+                  onClick={() => (setCurrentChannelId(id), setInputFocus(true))}
+                >                  
+                  # {name}                  
+                </Button>
+
+                <DropdownButton
+                  bsPrefix={
+                    currentChannelId === id
+                      ? "btn btn-secondary"
+                      : "btn btn-light"
+                  }
+                  as={ButtonGroup}
+                  id="bg-nested-dropdown"
+                >
+                  <Dropdown.Item id={id} onClick={handleRemove}>
+                    {t("dropdownBtn.removeBtn")}
+                  </Dropdown.Item>
+                  <Dropdown.Item id={id} onClick={handleRename}>
+                    {t("dropdownBtn.renameBtn")}
+                  </Dropdown.Item>
+                </DropdownButton>
+              </ButtonGroup>
+            </div>
           );
-        })}
-      </Nav>
+        }
+        return (
+          <Button
+            key={id}
+            type="submit"
+            eventkey={id}
+            onClick={() => (setCurrentChannelId(id), setInputFocus(true))}
+          >
+            # {name}
+          </Button>
+        );
+      })}
+      {/* </Nav> */}
     </div>
   );
 };
